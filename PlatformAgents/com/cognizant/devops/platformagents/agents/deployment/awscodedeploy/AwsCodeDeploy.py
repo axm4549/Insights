c4fb98c09515a0c0be183ffa3c2d6ead875e5604
Arpita Mathur <39963095+axm4549@users.noreply.github.com>
2018-06-22 17:06:47 +0530

    Update AwsCodeDeploy.py

--- PlatformAgents/com/cognizant/devops/platformagents/agents/deployment/awscodedeploy/AwsCodeDeploy.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/deployment/awscodedeploy/AwsCodeDeploy.py
@@ -21,66 +21,57 @@ class AwsCodeDeploy(BaseAgent):
             since = int(time.mktime(time.strptime(since,pattern)))
             lastUpdated = since
         client = boto3.client('codedeploy')
-        deployment = {
-           'Spot' : ['Dev','QA'],
-           'SWIFT' : ['QA'],
-           'test' : ['Dev']
-        }
-        deploystr = ast.literal_eval(json.dumps(deployment))
         getlist = []
-        deployIDlist = []
-        tracking_data = []
-        for app in deploystr.keys():
-            for groups in deploystr[app]:
-                response = client.list_deployments(
-                applicationName=app,
-                deploymentGroupName=groups
-                )
-                #print response
-                getlist = [[str(deployments) for deployments in response['deployments']] for deployments in response]
-                getlist = [item for items in getlist for item in items]
-                deployId=list(set(getlist))
-                for n in deployId:
-                    injectData = {}
-                    string = {}
-                    client = boto3.client('codedeploy')
-                    deploy = client.get_deployment(
-                    deploymentId=n
-                    )
-                    date = str(deploy['deploymentInfo']['createTime'])
-                    date = parser.parse(date)
-                    date = date.strftime('%Y-%m-%dT%H:%M:%S')
-                    pattern = '%Y-%m-%dT%H:%M:%S'
-                    date = int(time.mktime(time.strptime(date,pattern)))
-                    if since == None or date > since:
-                        injectData['status'] = str(deploy['deploymentInfo']['status'])
-                        injectData['applicationName'] = str(deploy['deploymentInfo']['applicationName'])
-                        injectData['deploymentId'] = str(deploy['deploymentInfo']['deploymentId'])
-                        injectData['deployType'] = str(deploy['deploymentInfo']['deploymentStyle']['deploymentType'])
-                        injectData['deploymentGroupName'] = str(deploy['deploymentInfo']['deploymentGroupName'])
-                        injectData['createTime'] = str(deploy['deploymentInfo']['createTime'])
-                        start = str(deploy['deploymentInfo']['createTime'])
-                        start = parser.parse(start)
-                        start_e = start.strftime('%Y-%m-%dT%H:%M:%S')
-                        start_f = start.strftime('%Y-%m-%d')
-                        injectData['startTime'] = start_f
-                        pattern = '%Y-%m-%dT%H:%M:%S'
-                        epoch = int(time.mktime(time.strptime(start_e,pattern)))
-                        injectData['startTimeepoch'] = epoch
-                        complete = str(deploy['deploymentInfo']['completeTime'])
-                        complete = parser.parse(complete)
-                        complete = complete.strftime('%Y-%m-%dT%H:%M:%S')
-                        epoch = int(time.mktime(time.strptime(complete,pattern)))
-                        injectData['completionTime'] = epoch
-                        injectData['lastUpdated'] = lastUpdated
-                        string = ast.literal_eval(json.dumps(injectData))
-                        tracking_data.append(string)
-                        seq = [x['createTime'] for x in tracking_data]
-                        fromDateTime = max(seq)
-                        fromDateTime = parser.parse(fromDateTime)
-                        fromDateTime = fromDateTime.strftime('%Y-%m-%dT%H:%M:%S')
-                    else:
-                        fromDateTime = lastUpdated
+        tracking_data =[]
+        response = client.list_deployments()
+        res = response.get('nextToken')
+        response = client.list_deployments(
+             nextToken=res
+             )
+        getlist = [[str(deployments) for deployments in response['deployments']] for deployments in response]
+        getlist = [item for items in getlist for item in items]
+        deployId=list(set(getlist))
+        for n in deployId:
+            injectData = {}
+            string = {}
+            client = boto3.client('codedeploy')
+            deploy = client.get_deployment(
+            deploymentId=n
+            )
+            date = str(deploy['deploymentInfo']['createTime'])
+            date = parser.parse(date)
+            date = date.strftime('%Y-%m-%dT%H:%M:%S')
+           # pattern = '%Y-%m-%dT%H:%M:%S'
+           # date = int(time.mktime(time.strptime(date,pattern)))
+            if since == None or date > since:
+                injectData['status'] = str(deploy['deploymentInfo']['status'])
+                injectData['applicationName'] = str(deploy['deploymentInfo']['applicationName'])
+                injectData['deploymentId'] = str(deploy['deploymentInfo']['deploymentId'])
+                injectData['deployType'] = str(deploy['deploymentInfo']['deploymentStyle']['deploymentType'])
+                injectData['deploymentGroupName'] = str(deploy['deploymentInfo']['deploymentGroupName'])
+                injectData['createTime'] = str(deploy['deploymentInfo']['createTime'])
+                start = str(deploy['deploymentInfo']['createTime'])
+                start = parser.parse(start)
+                start_e = start.strftime('%Y-%m-%dT%H:%M:%S')
+                start_f = start.strftime('%Y-%m-%d')
+                injectData['startTime'] = start_f
+                pattern = '%Y-%m-%dT%H:%M:%S'
+                epoch = int(time.mktime(time.strptime(start_e,pattern)))
+                injectData['startTimeepoch'] = epoch
+                complete = str(deploy['deploymentInfo']['completeTime'])
+                complete = parser.parse(complete)
+                complete = complete.strftime('%Y-%m-%dT%H:%M:%S')
+                epoch = int(time.mktime(time.strptime(complete,pattern)))
+                injectData['completionTime'] = complete
+                injectData['lastUpdated'] = lastUpdated
+                string = ast.literal_eval(json.dumps(injectData))
+                tracking_data.append(string)
+                seq = [x['completionTime'] for x in tracking_data]
+                fromDateTime = max(seq)
+               # fromDateTime = parser.parse(fromDateTime)
+               # fromDateTime = fromDateTime.strftime('%Y-%m-%dT%H:%M:%S')
+            else:
+                fromDateTime = lastUpdated
         self.tracking["lastupdated"] = fromDateTime
         if tracking_data!=[]:
             self.publishToolsData(tracking_data)
