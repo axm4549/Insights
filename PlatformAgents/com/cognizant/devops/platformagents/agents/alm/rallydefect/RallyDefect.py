a669b5b0267a2cf88b8dbf3abfe4743c17caf983
Arpita Mathur <39963095+axm4549@users.noreply.github.com>
2018-06-22 17:03:43 +0530

    Update RallyDefect.py

--- PlatformAgents/com/cognizant/devops/platformagents/agents/alm/rallydefect/RallyDefect.py
+++ PlatformAgents/com/cognizant/devops/platformagents/agents/alm/rallydefect/RallyDefect.py
@@ -31,73 +31,72 @@ class RallyDefect(BaseAgent):
         userid = self.config.get("userid", '')
         passwd = self.config.get("passwd", '')
         baseUrl = self.config.get("baseUrl", '')
-        releaseurl = self.config.get("WarriorsRelease", '')
+        Warriors = self.config.get("WarriorsRelease", '')
+        incredibles = self.config.get("incredibles", '')
         startFrom = self.config.get("startFrom", '')
         startFrom = parser.parse(startFrom)
         startFrom = startFrom.strftime('%Y-%m-%dT%H:%M:%S')
         responseTemplate = self.getResponseTemplate()
-        release = self.getResponse(releaseurl, 'GET', userid, passwd, None)
-        for url in release["QueryResult"]["Results"]:
-            data = []
-            relname = str(url['_refObjectName'])
-            name="\""+quote(relname)+"\""
-            since = self.tracking.get(relname,None)
-            if since == None:
-                lastUpdated = startFrom
-            else:
-                since = parser.parse(since)
-                since = since.strftime('%Y-%m-%d')
-                lastUpdated = since
-            urlappend_release_name="Release.Name = "+name
-            releaseName=urlappend_release_name
-            #url = "https://rally1.rallydev.com/slm/webservice/v2.0/workspace/13785475169"+"&query=("+releaseName+")"
-            hierachiesUrl = baseUrl+"defect?query=("+releaseName+")&pagesize=30&query=(LastUpdateDate>"+lastUpdated+")"
-            hierachies = self.getResponse(hierachiesUrl, 'GET', userid, passwd, None)
-            for hierarchy in hierachies["QueryResult"]["Results"]:
-                injectData = {}
-                reference = hierarchy['_ref']
-                ref = self.getResponse(reference, 'GET', userid, passwd, None)
-               # inject data is used as sample here update with actual values needed if any
-                date = ref['Defect']['LastUpdateDate']
-                date = parser.parse(date)
-                date = date.strftime('%Y-%m-%d')
-                if since == None or date > since:
-                    injectData['Status'] = ref['Defect']['State']
-                    # injectData['Defect'] = "Defect"
-                    Closed = ref['Defect']['ClosedDate']
-                    if Closed is not None:
-                        Closed = parser.parse(Closed)
-                        Closed = Closed.strftime('%Y-%m-%d')
-                        injectData['ClosedDate'] = Closed
-                    else:
-                        injectData['ClosedDate'] = Closed
-                    injectData['ID'] = ref['Defect']['FormattedID']
-                    if ref['Defect']['Iteration']:
-                        injectData['Iteration'] = ref['Defect']['Iteration']['_refObjectName']
-                    injectData['Release'] = relname
-                    injectData['Environment'] = ref['Defect']['Environment']
-                    check = ref['Defect']['CreationDate']
-                    if check is not None:
-                        check = parser.parse(check)
-                        check = check.strftime('%Y-%m-%d')
-                        injectData['OpenedDate'] = check
-                    else:
-                        injectData['OpenedDate'] = check
-                    Update = ref['Defect']['LastUpdateDate']
-                    Update = parser.parse(Update)
-                    Update = Update.strftime('%Y-%m-%d')
-                    injectData['LastUpdateDate'] = Update
-                    injectData['Project'] = ref['Defect']['Project']['_refObjectName']
-                    data += self.parseResponse(responseTemplate, hierarchy, injectData)
-                    seq = [x['LastUpdateDate'] for x in data]
-                    fromDateTime = max(seq)
-                    fromDateTime = parser.parse(fromDateTime)
-                    fromDateTime = fromDateTime.strftime('%d/%m/%Y')
+        project = [Warriors,incredibles]
+        for link in project:
+            release = self.getResponse(link, 'GET', userid, passwd, None)
+            for url in release["QueryResult"]["Results"]:
+                data = []
+                relname = str(url['_refObjectName'])
+                name="\""+quote(relname)+"\""
+                since = self.tracking.get(relname,None)
+                if since == None:
+                    lastUpdated = startFrom
                 else:
-                    fromDateTime = lastUpdated
-            if len(hierachies)>0:
-                self.tracking[relname] = fromDateTime
-                self.publishToolsData(data)
-                self.updateTrackingJson(self.tracking)
+                    since = parser.parse(since)
+                    since = since.strftime('%Y-%m-%dT%H:%M:%S')
+                    lastUpdated = since
+                urlappend_release_name="Release.Name = "+name
+                releaseName=urlappend_release_name
+                hierachiesUrl = baseUrl+"defect?query=("+releaseName+")&pagesize=2000&query=(LastUpdateDate>"+lastUpdated+")"
+                hierachies = self.getResponse(hierachiesUrl, 'GET', userid, passwd, None)
+                for hierarchy in hierachies["QueryResult"]["Results"]:
+                    injectData = {}
+                    reference = hierarchy['_ref']
+                    reference = reference+"?pagesize=2000"
+                    ref = self.getResponse(reference, 'GET', userid, passwd, None)
+               # inject data is used as sample here update with actual values needed if any
+                    date = ref['Defect']['LastUpdateDate']
+                    date = parser.parse(date)
+                    date = date.strftime('%Y-%m-%dT%H:%M:%S')
+                    if since == None or date > since:
+                        injectData['Status'] = ref['Defect']['State']
+                        Closed = ref['Defect']['ClosedDate']
+                        if Closed is not None:
+                            Closed = parser.parse(Closed)
+                            Closed = Closed.strftime('%Y-%m-%d')
+                            injectData['ClosedDate'] = Closed
+                        else:
+                            injectData['ClosedDate'] = Closed
+                        injectData['ID'] = ref['Defect']['FormattedID']
+                        if ref['Defect']['Iteration']:
+                            injectData['Iteration'] = ref['Defect']['Iteration']['_refObjectName']
+                        injectData['Release'] = relname
+                        injectData['Environment'] = ref['Defect']['Environment']
+                        check = ref['Defect']['CreationDate']
+                        if check is not None:
+                            check = parser.parse(check)
+                            check = check.strftime('%Y-%m-%d')
+                            injectData['OpenedDate'] = check
+                        else:
+                            injectData['OpenedDate'] = check
+                        injectData['LastUpdateDate'] = ref['Defect']['LastUpdateDate']
+                        injectData['Project'] = ref['Defect']['Project']['_refObjectName']
+                        data.append(injectData)
+                        seq = [x['LastUpdateDate'] for x in data]
+                        fromDateTime = max(seq)
+                        fromDateTime = parser.parse(fromDateTime)
+                        fromDateTime = fromDateTime.strftime('%Y-%m-%dT%H:%M:%S')
+                    else:
+                        fromDateTime = lastUpdated
+                if data!=[]:
+                    self.tracking[relname] = fromDateTime
+                    self.publishToolsData(data)
+                    self.updateTrackingJson(self.tracking)
 if __name__ == "__main__":
     RallyDefect()
